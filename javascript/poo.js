6e46739b5d3d3e853ffb5703676fc9cd0dd172b1
Gabriel Lima <gabriel.lima2307@uol.com.br>
2023-04-11 15:00:43 -0300

    ok

---
+++ javascript/poo.js
@@ -0,0 +1,52 @@
+let produto1 = {
+    Quantidade: 2,
+    name: "Capacete",
+    tempo: "2 anos",
+}
+
+let produto2 = {
+    Quantidade: 4,
+    name: "cinto",
+    tempo: "5 anos"
+}
+
+let produto3 = {
+    Quantidade: 3,
+    name: "chocolate",
+    tempo: "2025"
+}
+
+const product = [
+    produto1,
+    produto2,
+    produto3,
+]
+
+let Item = {
+    valor: 120,
+    produto1,
+}
+
+let Item2 = {
+    valor2: 60,
+    produto2
+}
+
+let Item3 = {
+    valor3: 18,
+    produto3
+}
+
+const item = [
+    Item,
+    Item2,
+    Item3,
+]
+
+const valores = function(Item, Item2, Item3){
+    soma = valor + valor2 + valor3
+    return soma
+}
+
+console.log(soma)
+
